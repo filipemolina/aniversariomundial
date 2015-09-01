adad0cfaaefae96728e51da7673c9635d1f1008e
Filipe Molina <filipemolina@live.com>
2015-09-01 16:15:21 -0300

    Ajustes

--- wp-content/themes/aniversario-mundial/header.php
+++ wp-content/themes/aniversario-mundial/header.php
@@ -14,6 +14,9 @@
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
   	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
 
   <!--[if lt IE 9]>
@@ -27,13 +30,25 @@
 
 	<!-- Menu Principal -->
 	
-	<nav class="navbar navbar-default">
+	<nav class="navbar navbar-inverse">
 
 		<div class="container">
 
 			<div class="navbar-header">
 
 				<a class="navbar-brand" href="#"><img alt="Logo" src="<?php bloginfo('template_url'); ?>/img/menu/logo.png"></a>
+
+			</div>
+
+			<div class="collapse navbar-collapse" id="menu-principal">
+				
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="" class="openSans branca">OFERTAS</a></li>
+					<li><a href="" class="openSans branca">CAMPANHA</a></li>
+					<li><a href="" class="openSans branca">#ÉHOJEMUNDIAL</a></li>
+					<li><a href="" class="openSans branca">DOWNLOADS</a></li>
+					<li><a href="" class="openSans branca">MAKING OF</a></li>
+				</ul>
 
 			</div>
 
