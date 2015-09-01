4ba2fb3f683d4d634903f5c57f4c48c9a30d2c1e
Filipe Molina <filipemolina@live.com>
2015-09-01 15:50:06 -0300

    Bootstrap

--- wp-content/themes/aniversario-mundial/footer.php
+++ wp-content/themes/aniversario-mundial/footer.php
@@ -1,5 +1,8 @@
 	
 	<?php wp_footer(); ?>
+	
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 	<script src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
 
--- wp-content/themes/aniversario-mundial/header.php
+++ wp-content/themes/aniversario-mundial/header.php
@@ -2,13 +2,19 @@
 
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+ 	<meta charset="utf-8">
 
-  <title>The HTML5 Herald</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
+	<title>The HTML5 Herald</title>
+	<meta name="description" content="The HTML5 Herald">
+	<meta name="author" content="SitePoint">
 
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+  	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+  	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
 
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -20,14 +26,14 @@
 <body>
 
 	<!-- Menu Principal -->
-
+	
 	<nav class="navbar navbar-default">
 
 		<div class="container">
 
 			<div class="navbar-header">
 
-				<a class="navbar-brand" href="#"><img alt="Logo" src="<?php bloginfo('template_url'); ?>"></a>
+				<a class="navbar-brand" href="#"><img alt="Logo" src="<?php bloginfo('template_url'); ?>/img/menu/logo.png"></a>
 
 			</div>
 
