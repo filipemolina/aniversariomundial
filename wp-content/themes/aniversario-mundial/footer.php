9a643c65633ac954c391f44fa1f9f14199181ac8
Kleiton Dias <kleitondias.ti@gmail.com>
2015-09-01 16:15:56 -0300

    nada

---
+++ wp-content/themes/aniversario-mundial/footer.php
@@ -0,0 +1,10 @@
+
+<footer>
+	<div class="container">
+		<div class="social">
+			<a class="facebook_footer" href="#"></a>
+			<a class="twitter_footer" href="#"></a>
+			<a class="instagram_footer" href="#"></a>
+		</div>
+	</div>
+</footer>
