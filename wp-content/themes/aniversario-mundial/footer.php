1deeb46f92c5dd4193c7f3e013b04e7e71cfca2e
Kleiton Dias <kleitondias.ti@gmail.com>
2015-09-01 17:48:01 -0300

    footer

--- wp-content/themes/aniversario-mundial/footer.php
+++ wp-content/themes/aniversario-mundial/footer.php
@@ -2,11 +2,15 @@
 
 <footer>
 	<div class="container">
-		<div class="social">
+		<div class="social col-md-3">
 			<a class="facebook_footer" href="#"></a>
 			<a class="twitter_footer" href="#"></a>
 			<a class="instagram_footer" href="#"></a>
 		</div>
+		<div class="copyright col-md-6">
+			<p>SUPERMERCADOS MUNDIAL LTDA &copy; 2011 - TODOS OS DIREITOS RESERVADOS.</p>
+		</div>
+		<div class="logo_footer col-md-3"><img src="<?php bloginfo('template_url'); ?>/img/menu/logo.png" alt=""></div>
 	</div>
 </footer>
 
