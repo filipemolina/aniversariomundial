a0807a86b761a838c338c34d633eae064581e00a
Filipe Molina <filipemolina@live.com>
2015-09-03 19:25:38 -0300

    Botões das redes sociais

--- wp-content/themes/aniversario-mundial/footer.php
+++ wp-content/themes/aniversario-mundial/footer.php
@@ -17,7 +17,7 @@
 			<a class="instagram_footer" href="#"></a>
 		</div>
 		<div class="copyright col-md-6">
-			<p>SUPERMERCADOS MUNDIAL LTDA &copy; 2011 - TODOS OS DIREITOS RESERVADOS.</p>
+			<p>SUPERMERCADOS MUNDIAL LTDA &copy; 2015 - TODOS OS DIREITOS RESERVADOS.</p>
 		</div>
 		<div class="logo_footer col-md-3"><img src="<?php bloginfo('template_url'); ?>/img/menu/logo.png" alt=""></div>
 	</div>
@@ -35,6 +35,12 @@
 	<script src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
 
 	<script src="https://api.instagram.com/v1/tags/<?php echo $hashTag; ?>/media/recent?client_id=5e3ffdba6bf04f0a8840a7ce98e57223&callback=Log"></script>
+
+	<!-- Script necessário para se obter o botão Subscribe do Youtube -->
+	<script src="https://apis.google.com/js/platform.js"></script>
+
+	<!-- Script necessário para se obtero botão de Follow do Instagram -->
+	<script>(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src="//x.instagramfollowbutton.com/follow.js";s.parentNode.insertBefore(g,s);}(document,"script"));</script>
 
 </body>
 </html>
---
+++ wp-content/themes/aniversario-mundial/sections/redes-sociais.php
@@ -0,0 +1,19 @@
+<div class="redes-sociais">
+	
+	<div class="container">
+
+		<!-- Facebook -->
+
+		<div class="fb-like" data-layout="button"></div>
+
+		<!-- Instagram -->
+
+		<span class="ig-follow" data-id="1297f04781" data-handle="supermercadosmundial" data-count="false" data-size="medium" data-username="false"></span>
+		
+		<!-- Youtube -->
+
+		<div class="g-ytsubscribe" data-channelid="UC4DwfhdKs59sqDZRqJ1mrFg" data-layout="default" data-count="hidden"></div>
+
+	</div>
+
+</div>
