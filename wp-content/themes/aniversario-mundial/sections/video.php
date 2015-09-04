02c923728a8ecdbaaacc1dc5576e4eff7203b591
Filipe Molina <filipemolina@live.com>
2015-09-04 17:26:16 -0300

    Ajustes no vide e CSS

--- wp-content/themes/aniversario-mundial/sections/video.php
+++ wp-content/themes/aniversario-mundial/sections/video.php
@@ -1,1 +1,5 @@
-<iframe width="100%" height="810" src="https://www.youtube.com/embed/w9zUu19tTgA" frameborder="0" allowfullscreen></iframe>
+<div class="video">
+	
+	<iframe width="100%" height="810" src="https://www.youtube.com/embed/w9zUu19tTgA" frameborder="0" allowfullscreen></iframe>
+
+</div>
